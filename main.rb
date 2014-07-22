3db3f6ffdb0a5d2bf3b7ffbb32c22e43031406cd
youngorchen <youngorchen@gmail.com>
2014-07-22 20:32:02 +0800

    init test

--- main.rb
+++ main.rb
@@ -1,3 +1,10 @@
+require 'sinatra'
+require 'slim'
+require 'data_mapper'
+
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/development.db")
+
+
 class Task
   include DataMapper::Resource
   property :id,           Serial
